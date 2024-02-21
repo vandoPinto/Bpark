6a82afc093f74ddf8162bbd4a8860c162094c773
Vando Pinto <vandopintoo@gmail.com>
2024-02-21 16:04:07 -0300

    formato mobile

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,4 +6,15 @@ $(function () {
         dynamic: true
     });
     $("#datepicker, #datepicker2").datepicker();
+
+    $('#menu-toggle').click(function (e) {
+        e.stopPropagation(); // Evita que o evento de clique seja propagado para o documento
+        $('.nav-menu').toggleClass('active');
+    });
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.nav-menu').length && !$(e.target).is('.menu-hamburger')) {
+            $('.nav-menu').removeClass('active');
+        }
+    });
 });
